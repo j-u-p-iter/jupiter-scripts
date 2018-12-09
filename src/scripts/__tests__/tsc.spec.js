9505ea6902aeea4b1c4f5baf6fd5c190c3db58b1
kirill <pn79122821224@gmail.com>
2018-12-09 09:02:42 +0100

    tests(tsc build script): checks that script removes root dist folder

--- src/scripts/__tests__/tsc.spec.js
+++ src/scripts/__tests__/tsc.spec.js
@@ -4,6 +4,7 @@ const mockTsConfigJson = jest.fn();
 jest.mock("../../config/tsconfig.json", () => mockTsConfigJson());
 
 jest.mock("cross-spawn", () => ({ sync: jest.fn(() => ({})) }));
+jest.mock("rimraf", () => ({ sync: jest.fn() }));
 
 const runScript = () => require("../build/tsc");
 
@@ -12,11 +13,13 @@ describe("tsc script", () => {
   let setupTSConfig;
   let utils;
   let mockCrossSpawnSync;
+  let mockRimrafSync;
 
   beforeEach(() => {
     jest.resetModules();
 
     ({ sync: mockCrossSpawnSync } = require("cross-spawn"));
+    ({ sync: mockRimrafSync } = require("rimraf"));
 
     utils = require("../../utils");
 
@@ -27,12 +30,19 @@ describe("tsc script", () => {
     Object.assign(utils, { setupTSConfig, handleSpawnSignal });
   });
 
-  it("requires tsconfig.json and setups tsconfig.json", () => {
+  // we require config to include this config into result dist folder
+  it("requires tsconfig.json, setups tsconfig.json and removes root dist folder", () => {
     runScript();
 
-    // we require config to include this config into result dist folder
+    // requires tsconfig.json
     expect(mockTsConfigJson).toHaveBeenCalledTimes(1);
+
+    // setups tsconfig.json
     expect(utils.setupTSConfig).toHaveBeenCalledTimes(1);
+
+    // removes root dist folder
+    expect(mockRimrafSync).toHaveBeenCalledTimes(1);
+    expect(mockRimrafSync).toHaveBeenCalledWith(utils.fromRoot('dist'));
   });
 
   it("runs correct bin", () => {
