efdfb704d9f97a53e6d718346916f046ad8233ed
kirill <pn79122821224@gmail.com>
2018-12-15 16:38:53 +0100

    fix(deploy script): passes coverage data to coveralls

--- src/scripts/deploy.js
+++ src/scripts/deploy.js
@@ -5,7 +5,7 @@ const { resolveBin } = require("../utils");
 const { status } = spawn.sync(
   resolveBin("concurrently"),
   [
-    `echo installing coveralls && npx -p coveralls@3 -c 'echo running coveralls && coveralls'`,
+    `echo installing coveralls && npx -p coveralls@3 -c 'echo running coveralls && yarn run test --coverageReporters=text-lcov | coveralls'`,
     `echo installing semantic-release && npx -p semantic-release@15 -c 'echo running semantic-release && semantic-release'`
   ],
   { stdio: "inherit" }
